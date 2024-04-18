8d17f37020f74ee2e95273fdad83d2c1573511ff
Dicky-Fahriza <dickyfahriza46@gmail.com>
2024-04-18 11:34:26 +0700

    Initial Commit

---
+++ MontraDev/MontraDevApp.swift
@@ -0,0 +1,17 @@
+//
+//  MontraDevApp.swift
+//  MontraDev
+//
+//  Created by MacBook Pro on 18/04/24.
+//
+
+import SwiftUI
+
+@main
+struct MontraDevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
